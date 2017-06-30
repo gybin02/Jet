84397f6a1dd51fa1c1748ba15eaf4dba409fde55
zhengxiaobin <zhengxiaobin@linggan.com>
2017-06-30 09:47:12 +0800

    1. 新增自动授权

---
+++ jet_library/src/main/java/com/meiyou/jet/grant/Permissions.java
@@ -0,0 +1,12 @@
+package com.meiyou.jet.grant;
+
+/**
+ * Enum class to handle the different states
+ * of permissions since the PackageManager only
+ * has a granted and denied state.
+ */
+enum Permissions {
+    GRANTED,
+    DENIED,
+    NOT_FOUND
+}
