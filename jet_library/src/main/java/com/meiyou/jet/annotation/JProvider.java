de069f44402674128db17987e9417f4821dd5e3c
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-07 18:19:31 +0800

    1. 改为运行时，方便后续扩展；

--- jet_library/src/main/java/com/meiyou/jet/annotation/JProvider.java
+++ jet_library/src/main/java/com/meiyou/jet/annotation/JProvider.java
@@ -6,13 +6,16 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 标识类为JImplement实现类，没什么用，加了提示其他人不要删除该类
- *
+ * 标识类为JImplement实现类，提示其他人不要删除该类
+ * 用于混淆配置
  * @author zhengxiaobin@xiaoyouzi.com
  * @since 17/5/23
  */
+//RetentionPolicy.Source；只会存在Java文件中；
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JProvider {
     String value() default "";
 }
+
+
