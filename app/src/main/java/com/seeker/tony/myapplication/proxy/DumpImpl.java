867e04a4dded45c0878e1d08df76a8a1347dc9e4
zhengxiaobin <zhengxiaobin@linggan.com>
2017-07-07 18:22:20 +0800

    1. 新增测试类；

---
+++ app/src/main/java/com/seeker/tony/myapplication/proxy/DumpImpl.java
@@ -0,0 +1,25 @@
+package com.seeker.tony.myapplication.proxy;
+
+import android.util.Log;
+
+import com.meiyou.jet.annotation.JImplement;
+
+/**
+ * @author zhengxiaobin@xiaoyouzi.com
+ * @since 17/5/22
+ */
+@JImplement
+public class DumpImpl {
+
+    private static final String TAG = "TestImpl";
+    
+    public void test() {
+        Log.d(TAG, "test Method  invoke");
+    }
+
+    public String getValue() {
+        String str = "HelloWorld";
+        Log.d(TAG, "getValue Method  invoke： " + str);
+        return str;
+    }
+}
